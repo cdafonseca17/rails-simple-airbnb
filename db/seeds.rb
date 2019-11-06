a1e3ddea1e3fd1d13516dbe8dc360ddf46e61240
Caroline Dafonseca <dafonseca.ca@gmail.com>
2019-11-06 16:37:04 +0100

    Rails new with frontend and form gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+5.times do
+  flat = Flat.new(
+    name: Faker::Address.city,
+    address: Faker::Address.street_address,
+    description: Faker::Lorem.paragraph,
+    price_per_night: [50, 60, 80, 90].sample,
+    number_of_guests: [1, 2, 3, 4].sample
+  )
+  flat.save!
+end
+
